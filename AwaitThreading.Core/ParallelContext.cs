3e7dcbaa14b93ad2c86685c0af20b4bb89e17833
saltuk <saltukkos@gmail.com>
2023-07-30 21:05:58 +0200

    Make ParallelContext respect the threads change

--- AwaitThreading.Core/ParallelContext.cs
+++ AwaitThreading.Core/ParallelContext.cs
@@ -6,8 +6,7 @@ namespace AwaitThreading.Core;
 
 public readonly struct ParallelContext
 {
-    [ThreadStatic]
-    private static Stack<ParallelContext>? _parallelContexts;
+    private static readonly AsyncLocal<Stack<ParallelContext>> ParallelContexts = new();
 
     public ParallelContext(int id)
     {
@@ -18,17 +17,17 @@ public readonly struct ParallelContext
 
     public static ParallelContext? GetCurrentContext()
     {
-        return _parallelContexts?.Peek();
+        return ParallelContexts.Value?.Peek();
     }
 
     public static void PushContext(ParallelContext context)
     {
-        _parallelContexts ??= new Stack<ParallelContext>();
-        _parallelContexts.Push(context);
+        ParallelContexts.Value ??= new Stack<ParallelContext>();
+        ParallelContexts.Value.Push(context);
     }
 
     public static ParallelContext PopContext()
     {
-        return _parallelContexts!.Pop();
+        return ParallelContexts.Value!.Pop();
     }
 }
