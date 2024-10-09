5af218e0bf3588bfe7cb33a85abd0dc174f1c7f0
saltuk <saltukkos@gmail.com>
2024-10-09 08:23:37 +0200

    Use RangeWorker in AsParallelAsync

--- AwaitThreading.Enumerable/ListExtensions.cs
+++ AwaitThreading.Enumerable/ListExtensions.cs
@@ -10,17 +10,9 @@ public static class ListExtensions
 {
     public static async ParallelTask<ChunkEnumerable<T>> AsParallelAsync<T>(this List<T> list, int threadsCount)
     {
-        var chunkSize = (list.Count + threadsCount - 1) / threadsCount;
+        var rangeManager = new RangeManager(0, list.Count, 1, threadsCount);
         await new ForkingTask(threadsCount);
-        var id = ParallelContext.Id;
-        var start = chunkSize * id;
-        var end = chunkSize * (id + 1);
-        if (end > list.Count)
-        {
-            end = list.Count;
-        }
-
-        return new ChunkEnumerable<T>(list, start, end);
+        return new ChunkEnumerable<T>(list, rangeManager.RegisterNewWorker());
     }
     
     public static ParallelLazyAsyncEnumerable<T> AsParallel<T>(this List<T> list, int threadsCount)
